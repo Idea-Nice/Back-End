fef261e2a373ad62141c6d6acec2ea43fcdf0f26
yeboong99 <yechani9905@gmail.com>
2024-08-07 03:06:51 +0900

    씨발 설마

--- healax/src/main/java/com/example/healax/config/SecurityConfig.java
+++ healax/src/main/java/com/example/healax/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                             @Override
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                                 CorsConfiguration configuration = new CorsConfiguration();
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedOrigins(Collections.singletonList("http://13.124.95.115/"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
                                 configuration.setAllowedHeaders(Collections.singletonList("*"));
