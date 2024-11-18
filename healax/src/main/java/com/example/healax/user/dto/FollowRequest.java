ec63f980bc8f85ab296ff214d7f50cd234bd0165
yeboong99 <143702092+yeboong99@users.noreply.github.com>
2024-11-18 20:56:54 +0900

    main브랜치 리셋

--- healax/src/main/java/com/example/healax/user/dto/FollowRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.healax.user.dto;
-
-import lombok.Data;
-
-@Data
-public class FollowRequest {
-    private Long followerId;
-    private Long followingId;
-}
--- healax/src/main/java/com/example/healax/user/repository/UserVideoHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.healax.user.repository;
-
-import com.example.healax.user.entity.UserVideoHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserVideoHistoryRepository extends JpaRepository<UserVideoHistory, Long> {
-}
